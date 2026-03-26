f4168539d22eb2d8f3e03fd785cdab220663d578
ahmdngi <ahmdngi@hotmail.com>
2026-03-26 17:40:20 +0200

    fix

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -1,3 +1,41 @@
+const ensureFooter = () => {
+  const footerInner = document.querySelector(".md-footer-meta__inner");
+
+  if (!footerInner) {
+    return;
+  }
+
+  let copyright = footerInner.querySelector(".md-copyright");
+  if (!copyright) {
+    copyright = document.createElement("div");
+    copyright.className = "md-copyright";
+    footerInner.prepend(copyright);
+  }
+
+  copyright.innerHTML = `
+    <span class="haris-footer-copy">
+    Made with
+    <a href="https://zensical.org/" target="_blank" rel="noopener">
+      Zensical
+    </a>
+    </span>
+  `;
+
+  let social = footerInner.querySelector(".haris-footer-links");
+  if (!social) {
+    social = document.createElement("div");
+    social.className = "haris-footer-links";
+    footerInner.appendChild(social);
+  }
+
+  social.innerHTML = `
+    <a href="https://notes.ahmdngi.io/" target="_blank" rel="noopener" class="haris-footer-link">Home</a>
+    <a href="https://github.com/ahmdngi" target="_blank" rel="noopener" class="haris-footer-link">GitHub</a>
+    <a href="https://www.linkedin.com/in/ahmdngi/" target="_blank" rel="noopener" class="haris-footer-link">LinkedIn</a>
+    <a href="mailto:ahmdngi@hotmail.com" target="_blank" rel="noopener" class="haris-footer-link">Email</a>
+  `;
+};
+
 document$.subscribe(() => {
   let orb = document.querySelector(".floating-orb");
 
@@ -7,4 +45,6 @@ document$.subscribe(() => {
     orb.setAttribute("aria-hidden", "true");
     document.body.appendChild(orb);
   }
+
+  ensureFooter();
 });
